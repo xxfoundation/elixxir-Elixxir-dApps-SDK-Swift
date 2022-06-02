bcd5306e5267ef67ea2a65973e3b8368fb737d0a
Dariusz Rybicki <dariusz@elixxir.io>
2022-06-02 22:06:42 +0200

    Add NetworkFollowerStopper

--- Sources/ElixxirDAppsSDK/NetworkFollower.swift
+++ Sources/ElixxirDAppsSDK/NetworkFollower.swift
@@ -3,13 +3,15 @@ import Bindings
 public struct NetworkFollower {
   public var status: NetworkFollowerStatusProvider
   public var start: NetworkFollowerStarter
+  public var stop: NetworkFollowerStopper
 }
 
 extension NetworkFollower {
   public static func live(bindingsClient: BindingsClient) -> NetworkFollower {
     NetworkFollower(
       status: .live(bindingsClient: bindingsClient),
-      start: .live(bindingsClient: bindingsClient)
+      start: .live(bindingsClient: bindingsClient),
+      stop: .live(bindingsClient: bindingsClient)
     )
   }
 }
@@ -18,7 +20,8 @@ extension NetworkFollower {
 extension NetworkFollower {
   public static let failing = NetworkFollower(
     status: .failing,
-    start: .failing
+    start: .failing,
+    stop: .failing
   )
 }
 #endif
