18bc435a56b876e4f5535909d43777bc8b2f7ec4
Dariusz Rybicki <dariusz@elixxir.io>
2022-06-07 12:04:20 +0200

    Use Identity model in ContactFromIdentityProvider

--- Sources/ElixxirDAppsSDK/ContactFromIdentityProvider.swift
+++ Sources/ElixxirDAppsSDK/ContactFromIdentityProvider.swift
@@ -1,16 +1,21 @@
 import Bindings
 
 public struct ContactFromIdentityProvider {
-  public var get: (Data) throws -> Data
+  public var get: (Identity) throws -> Data
 
-  public func callAsFunction(identity: Data) throws -> Data {
+  public func callAsFunction(identity: Identity) throws -> Data {
     try get(identity)
   }
 }
 
 extension ContactFromIdentityProvider {
   public static func live(bindingsClient: BindingsClient) -> ContactFromIdentityProvider {
-    ContactFromIdentityProvider(get: bindingsClient.getContactFromIdentity(_:))
+    ContactFromIdentityProvider { identity in
+      let encoder = JSONEncoder()
+      let identityData = try encoder.encode(identity)
+      let contactData = try bindingsClient.getContactFromIdentity(identityData)
+      return contactData
+    }
   }
 }
 
