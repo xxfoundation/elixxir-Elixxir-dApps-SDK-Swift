a109e922caf6fe8919223d5226eaa4851b64baae
Dariusz Rybicki <dariusz@elixxir.io>
2022-07-20 11:58:58 +0100

    Add Cmix struct

---
+++ Sources/ElixxirDAppsSDK/Cmix.swift
@@ -0,0 +1,15 @@
+import Bindings
+
+public struct Cmix {
+  // TODO:
+}
+
+extension Cmix {
+  public static func live(_ bindingsCmix: BindingsCmix) -> Cmix {
+    Cmix()
+  }
+}
+
+extension Cmix {
+  public static let unimplemented = Cmix()
+}
