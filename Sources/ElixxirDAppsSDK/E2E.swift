97aa776edb8dec569293875476e0551ec076815d
Dariusz Rybicki <dariusz@elixxir.io>
2022-07-20 16:22:02 +0100

    Add E2E wrapper

---
+++ Sources/ElixxirDAppsSDK/E2E.swift
@@ -0,0 +1,15 @@
+import Bindings
+
+public struct E2E {
+  // TODO:
+}
+
+extension E2E {
+  public static func live(_ bindingsE2E: BindingsE2e) -> E2E {
+    E2E()
+  }
+}
+
+extension E2E {
+  public static let unimplemented = E2E()
+}
