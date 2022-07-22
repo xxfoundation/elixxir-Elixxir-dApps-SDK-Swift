3f4d0415117ef1b0b9d3be4d3b7f4fe24890269c
Dariusz Rybicki <dariusz@elixxir.io>
2022-07-22 13:03:17 +0100

    Add ConnectionIsAuthenticated functor

--- Sources/ElixxirDAppsSDK/Connection.swift
+++ Sources/ElixxirDAppsSDK/Connection.swift
@@ -1,6 +1,7 @@
 import Bindings
 
 public struct Connection {
+  public var isAuthenticated: ConnectionIsAuthenticated
   public var getId: ConnectionGetId
   public var getPartner: ConnectionGetPartner
   public var close: ConnectionClose
@@ -9,6 +10,7 @@ public struct Connection {
 extension Connection {
   public static func live(_ bindingsConnection: BindingsConnection) -> Connection {
     Connection(
+      isAuthenticated: .live(bindingsConnection),
       getId: .live(bindingsConnection),
       getPartner: .live(bindingsConnection),
       close: .live(bindingsConnection)
@@ -17,6 +19,7 @@ extension Connection {
 
   public static func live(_ bindingsConnection: BindingsAuthenticatedConnection) -> Connection {
     Connection(
+      isAuthenticated: .live(bindingsConnection),
       getId: .live(bindingsConnection),
       getPartner: .live(bindingsConnection),
       close: .live(bindingsConnection)
@@ -26,6 +29,7 @@ extension Connection {
 
 extension Connection {
   public static let unimplemented = Connection(
+    isAuthenticated: .unimplemented,
     getId: .unimplemented,
     getPartner: .unimplemented,
     close: .unimplemented
