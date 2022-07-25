1f0395fbc0d4c8e641f3fff7e712d6af11021c3c
Dariusz Rybicki <dariusz@elixxir.io>
2022-07-25 15:27:34 +0100

    Add E2EGetId functor

--- Sources/ElixxirDAppsSDK/E2E.swift
+++ Sources/ElixxirDAppsSDK/E2E.swift
@@ -1,15 +1,21 @@
 import Bindings
 
 public struct E2E {
+  public var getId: E2EGetId
+
   // TODO:
 }
 
 extension E2E {
   public static func live(_ bindingsE2E: BindingsE2e) -> E2E {
-    E2E()
+    E2E(
+      getId: .live(bindingsE2E: bindingsE2E)
+    )
   }
 }
 
 extension E2E {
-  public static let unimplemented = E2E()
+  public static let unimplemented = E2E(
+    getId: .unimplemented
+  )
 }
