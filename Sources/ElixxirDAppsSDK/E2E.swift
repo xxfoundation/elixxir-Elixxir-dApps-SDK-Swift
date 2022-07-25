6b09d25c02aa3c60026a6f9289ac095edf3c87b0
Dariusz Rybicki <dariusz@elixxir.io>
2022-07-25 15:32:28 +0100

    Add E2EPayloadSize functor

--- Sources/ElixxirDAppsSDK/E2E.swift
+++ Sources/ElixxirDAppsSDK/E2E.swift
@@ -3,6 +3,7 @@ import Bindings
 public struct E2E {
   public var getId: E2EGetId
   public var getReceptionId: E2EGetReceptionId
+  public var payloadSize: E2EPayloadSize
 
   // TODO:
 }
@@ -11,7 +12,8 @@ extension E2E {
   public static func live(_ bindingsE2E: BindingsE2e) -> E2E {
     E2E(
       getId: .live(bindingsE2E: bindingsE2E),
-      getReceptionId: .live(bindingsE2E: bindingsE2E)
+      getReceptionId: .live(bindingsE2E: bindingsE2E),
+      payloadSize: .live(bindingsE2E: bindingsE2E)
     )
   }
 }
@@ -19,6 +21,7 @@ extension E2E {
 extension E2E {
   public static let unimplemented = E2E(
     getId: .unimplemented,
-    getReceptionId: .unimplemented
+    getReceptionId: .unimplemented,
+    payloadSize: .unimplemented
   )
 }
