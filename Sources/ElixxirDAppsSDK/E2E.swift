c73cc2805bc8f61e06492cb427bc1fd6da6c83a8
Dariusz Rybicki <dariusz@elixxir.io>
2022-07-25 15:30:25 +0100

    Add E2EGetReceptionId functor

--- Sources/ElixxirDAppsSDK/E2E.swift
+++ Sources/ElixxirDAppsSDK/E2E.swift
@@ -2,6 +2,7 @@ import Bindings
 
 public struct E2E {
   public var getId: E2EGetId
+  public var getReceptionId: E2EGetReceptionId
 
   // TODO:
 }
@@ -9,13 +10,15 @@ public struct E2E {
 extension E2E {
   public static func live(_ bindingsE2E: BindingsE2e) -> E2E {
     E2E(
-      getId: .live(bindingsE2E: bindingsE2E)
+      getId: .live(bindingsE2E: bindingsE2E),
+      getReceptionId: .live(bindingsE2E: bindingsE2E)
     )
   }
 }
 
 extension E2E {
   public static let unimplemented = E2E(
-    getId: .unimplemented
+    getId: .unimplemented,
+    getReceptionId: .unimplemented
   )
 }
