0253bbef0d0626ea53c3d4ad24b8f3909cb7c9ed
Dariusz Rybicki <dariusz@elixxir.io>
2022-07-26 18:33:52 +0100

    Refactor ServiceProcessor callback

--- Sources/ElixxirDAppsSDK/Callbacks/ServiceProcessor.swift
+++ Sources/ElixxirDAppsSDK/Callbacks/ServiceProcessor.swift
@@ -2,20 +2,27 @@ import Bindings
 import XCTestDynamicOverlay
 
 public struct ServiceProcessor {
-  public typealias Process = (
-    _ message: Data,
-    _ receptionId: Data,
-    _ ephemeralId: Int64,
-    _ roundId: Int64
-  ) -> Void
+  public struct Callback: Equatable {
+    public init(message: Data, receptionId: Data, ephemeralId: Int64, roundId: Int64) {
+      self.message = message
+      self.receptionId = receptionId
+      self.ephemeralId = ephemeralId
+      self.roundId = roundId
+    }
 
-  public init(serviceTag: String, process: @escaping Process) {
+    public var message: Data
+    public var receptionId: Data
+    public var ephemeralId: Int64
+    public var roundId: Int64
+  }
+
+  public init(serviceTag: String, process: @escaping (Callback) -> Void) {
     self.serviceTag = serviceTag
     self.process = process
   }
 
   public var serviceTag: String
-  public var process: Process
+  public var process: (Callback) -> Void
 }
 
 extension ServiceProcessor {
@@ -41,7 +48,12 @@ extension ServiceProcessor {
         guard let receptionId = receptionId else {
           fatalError("BindingsProcessor.process received `nil` receptionId")
         }
-        serviceProcessor.process(message, receptionId, ephemeralId, roundId)
+        serviceProcessor.process(Callback(
+          message: message,
+          receptionId: receptionId,
+          ephemeralId: ephemeralId,
+          roundId: roundId
+        ))
       }
 
       func string() -> String {
