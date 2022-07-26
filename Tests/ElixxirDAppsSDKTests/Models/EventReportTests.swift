f01aa47c42a205b7dfa6e315ed0a73c0b13b286a
Dariusz Rybicki <dariusz@elixxir.io>
2022-07-26 16:54:16 +0100

    Add EventReport model

---
+++ Tests/ElixxirDAppsSDKTests/Models/EventReportTests.swift
@@ -0,0 +1,35 @@
+import CustomDump
+import XCTest
+@testable import ElixxirDAppsSDK
+
+final class EventReportTests: XCTestCase {
+  func testCoding() throws {
+    let priority: Int = 1
+    let category = "Test Events"
+    let eventType = "Ping"
+    let details = "This is an example of an event report"
+    let jsonString = """
+    {
+      "Priority": \(priority),
+      "Category": "\(category)",
+      "EventType": "\(eventType)",
+      "Details": "\(details)"
+    }
+    """
+    let jsonData = jsonString.data(using: .utf8)!
+
+    let report = try EventReport.decode(jsonData)
+
+    XCTAssertNoDifference(report, EventReport(
+      priority: priority,
+      category: category,
+      eventType: eventType,
+      details: details
+    ))
+
+    let encodedReport = try report.encode()
+    let decodedReport = try EventReport.decode(encodedReport)
+
+    XCTAssertNoDifference(decodedReport, report)
+  }
+}
