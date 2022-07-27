924e52177c96aae5a6f7e5bc6295033423f201b3
Dariusz Rybicki <dariusz@elixxir.io>
2022-07-27 15:18:15 +0100

    Update NetworkFollowerStatus model

--- Sources/ElixxirDAppsSDK/Models/NetworkFollowerStatus.swift
+++ Sources/ElixxirDAppsSDK/Models/NetworkFollowerStatus.swift
@@ -1,6 +1,5 @@
 public enum NetworkFollowerStatus: Equatable {
   case stopped
-  case starting
   case running
   case stopping
   case unknown(code: Int)
@@ -10,7 +9,6 @@ extension NetworkFollowerStatus {
   public init(rawValue: Int) {
     switch rawValue {
     case 0: self = .stopped
-    case 1_000: self = .starting
     case 2_000: self = .running
     case 3_000: self = .stopping
     case let code: self = .unknown(code: code)
@@ -20,7 +18,6 @@ extension NetworkFollowerStatus {
   public var rawValue: Int {
     switch self {
     case .stopped: return 0
-    case .starting: return 1_000
     case .running: return 2_000
     case .stopping: return 3_000
     case .unknown(let code): return code
