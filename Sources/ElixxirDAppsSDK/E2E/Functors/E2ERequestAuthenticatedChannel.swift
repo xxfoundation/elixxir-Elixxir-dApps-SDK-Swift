f66d4801e50d819639c83985f526b03f678e0535
Dariusz Rybicki <dariusz@elixxir.io>
2022-07-27 11:24:35 +0100

    Update E2ERequestAuthenticatedChannel functor

--- Sources/ElixxirDAppsSDK/E2E/Functors/E2ERequestAuthenticatedChannel.swift
+++ Sources/ElixxirDAppsSDK/E2E/Functors/E2ERequestAuthenticatedChannel.swift
@@ -2,23 +2,23 @@ import Bindings
 import XCTestDynamicOverlay
 
 public struct E2ERequestAuthenticatedChannel {
-  public var run: (Data, String) throws -> Int64
+  public var run: (Data, [Fact]) throws -> Int64
 
   public func callAsFunction(
     partnerContact: Data,
-    myFactsString: String
+    myFacts: [Fact]
   ) throws -> Int64 {
-    try run(partnerContact, myFactsString)
+    try run(partnerContact, myFacts)
   }
 }
 
 extension E2ERequestAuthenticatedChannel {
   public static func live(_ bindingsE2E: BindingsE2e) -> E2ERequestAuthenticatedChannel {
-    E2ERequestAuthenticatedChannel { partnerContact, myFactsString in
+    E2ERequestAuthenticatedChannel { partnerContact, myFacts in
       var roundId: Int64 = 0
       try bindingsE2E.request(
         partnerContact,
-        myFactsString: myFactsString,
+        factsListJson: try myFacts.encode(),
         ret0_: &roundId
       )
       return roundId
