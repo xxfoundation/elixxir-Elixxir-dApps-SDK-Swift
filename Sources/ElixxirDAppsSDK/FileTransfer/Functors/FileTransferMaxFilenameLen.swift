de7b003aca066d3f644661814b472e08f1121392
Dariusz Rybicki <dariusz@elixxir.io>
2022-07-29 14:46:09 +0100

    Wrap FileTransfer object

---
+++ Sources/ElixxirDAppsSDK/FileTransfer/Functors/FileTransferMaxFilenameLen.swift
@@ -0,0 +1,22 @@
+import Bindings
+import XCTestDynamicOverlay
+
+public struct FileTransferMaxFilenameLen {
+  public var run: () -> Int
+
+  public func callAsFunction() -> Int {
+    run()
+  }
+}
+
+extension FileTransferMaxFilenameLen {
+  public static func live(_ bindingsFileTransfer: BindingsFileTransfer) -> FileTransferMaxFilenameLen {
+    FileTransferMaxFilenameLen(run: bindingsFileTransfer.maxFileNameLen)
+  }
+}
+
+extension FileTransferMaxFilenameLen {
+  public static let unimplemented = FileTransferMaxFilenameLen(
+    run: XCTUnimplemented("\(Self.self)")
+  )
+}
