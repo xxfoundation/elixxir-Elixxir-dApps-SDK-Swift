50e7d2261ce6282608b831f7226e6cb66714b9b6
Dariusz Rybicki <dariusz@elixxir.io>
2022-08-01 12:21:21 +0100

    Add SearchUD functor

---
+++ Sources/ElixxirDAppsSDK/Functors/SearchUD.swift
@@ -0,0 +1,46 @@
+import Bindings
+import XCTestDynamicOverlay
+
+public struct SearchUD {
+  public var run: (Int, Data, [Fact], Data, UdSearchCallback) throws -> SingleUseSendReport
+
+  public func callAsFunction(
+    e2eId: Int,
+    udContact: Data,
+    facts: [Fact],
+    singleRequestParamsJSON: Data,
+    callback: UdSearchCallback
+  ) throws -> SingleUseSendReport {
+    try run(e2eId, udContact, facts, singleRequestParamsJSON, callback)
+  }
+}
+
+extension SearchUD {
+  public static let live = SearchUD {
+    e2eId, udContact, facts, singleRequestParamsJSON, callback in
+
+    var error: NSError?
+    let reportData = BindingsSearchUD(
+      e2eId,
+      udContact,
+      callback.makeBindingsUdSearchCallback(),
+      try facts.encode(),
+      singleRequestParamsJSON,
+      &error
+    )
+    if let error = error {
+      throw error
+    }
+    guard let reportData = reportData else {
+      fatalError("BindingsSearchUD returned `nil` without providing error")
+    }
+    return try SingleUseSendReport.decode(reportData)
+  }
+}
+
+extension SearchUD {
+  public static let unimplemented = SearchUD(
+    run: XCTUnimplemented("\(Self.self)")
+  )
+}
+
