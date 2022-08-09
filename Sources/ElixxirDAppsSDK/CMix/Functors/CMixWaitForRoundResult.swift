abf737b2671a178f1b63b494c8fc8d3372624304
Dariusz Rybicki <dariusz@elixxir.io>
2022-08-09 19:12:17 +0100

    Update CMixWaitForRoundResult

--- Sources/ElixxirDAppsSDK/CMix/Functors/CMixWaitForRoundResult.swift
+++ Sources/ElixxirDAppsSDK/CMix/Functors/CMixWaitForRoundResult.swift
@@ -2,22 +2,22 @@ import Bindings
 import XCTestDynamicOverlay
 
 public struct CMixWaitForRoundResult {
-  public var run: (E2ESendReport, Int, MessageDeliveryCallback) throws -> Void
+  public var run: (Data, Int, MessageDeliveryCallback) throws -> Void
 
   public func callAsFunction(
-    report: E2ESendReport,
+    roundList: Data,
     timeoutMS: Int,
     callback: MessageDeliveryCallback
   ) throws {
-    try run(report, timeoutMS, callback)
+    try run(roundList, timeoutMS, callback)
   }
 }
 
 extension CMixWaitForRoundResult {
   public static func live(_ bindingsCMix: BindingsCmix) -> CMixWaitForRoundResult {
-    CMixWaitForRoundResult { report, timeoutMS, callback in
+    CMixWaitForRoundResult { roundList, timeoutMS, callback in
       try bindingsCMix.wait(
-        forRoundResult: try report.encode(),
+        forRoundResult: roundList,
         mdc: callback.makeBindingsMessageDeliveryCallback(),
         timeoutMS: timeoutMS
       )
