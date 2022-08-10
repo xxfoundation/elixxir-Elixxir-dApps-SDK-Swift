c7188e3a1c939f06024f2acd89757f8d885b81ab
Dariusz Rybicki <dariusz@elixxir.io>
2022-08-10 22:04:06 +0100

    Update SingleUseResponseReport model

--- Sources/ElixxirDAppsSDK/Models/SingleUseResponseReport.swift
+++ Sources/ElixxirDAppsSDK/Models/SingleUseResponseReport.swift
@@ -4,40 +4,29 @@ public struct SingleUseResponseReport: Equatable {
   public init(
     rounds: [Int],
     payload: Data,
-    receptionId: ReceptionId,
+    ephId: Int64,
+    receptionId: Data,
     error: String?
   ) {
     self.rounds = rounds
     self.payload = payload
+    self.ephId = ephId
     self.receptionId = receptionId
     self.error = error
   }
 
   public var rounds: [Int]
   public var payload: Data
-  public var receptionId: ReceptionId
+  public var ephId: Int64
+  public var receptionId: Data
   public var error: String?
-}
-
-extension SingleUseResponseReport {
-  public struct ReceptionId: Equatable {
-    public init(
-      ephId: [Int],
-      source: Data
-    ) {
-      self.ephId = ephId
-      self.source = source
-    }
-
-    public var ephId: [Int]
-    public var source: Data
-  }
 }
 
 extension SingleUseResponseReport: Codable {
   enum CodingKeys: String, CodingKey {
     case rounds = "Rounds"
     case payload = "Payload"
+    case ephId = "EphID"
     case receptionId = "ReceptionID"
     case error = "Err"
   }
@@ -48,12 +37,5 @@ extension SingleUseResponseReport: Codable {
 
   public func encode() throws -> Data {
     try JSONEncoder().encode(self)
-  }
-}
-
-extension SingleUseResponseReport.ReceptionId: Codable {
-  enum CodingKeys: String, CodingKey {
-    case ephId = "EphId"
-    case source = "Source"
   }
 }
