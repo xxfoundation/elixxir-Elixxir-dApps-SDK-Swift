ccc6db8b4c248932a5c54f76b8baffe2a0957d0c
Dariusz Rybicki <dariusz@elixxir.io>
2022-08-24 11:00:12 +0100

    Remove outdated example project

--- Example/Package.swift
+++
@@ -1,4 +0,0 @@
-// swift-tools-version:5.6
-// This file makes Xcode doesn't display this directory inside swift package.
-import PackageDescription
-let package = Package(name: "", products: [], targets: [])
