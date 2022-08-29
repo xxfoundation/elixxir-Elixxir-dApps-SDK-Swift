b6bc7c704a5de01ef35dce5f54dbfc4583dbe904
Dariusz Rybicki <dariusz@elixxir.io>
2022-08-29 15:02:28 +0100

    Update run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
 set -e
 
-echo "\n\033[1;32m▶ Running package tests on iOS Simulator...\033[0m"
-set -o pipefail && xcodebuild -scheme 'elixxir-dapps-sdk-swift-Package' -sdk iphonesimulator -destination 'platform=iOS Simulator,OS=15.5,name=iPhone 13' test | ./xcbeautify
+if [ ! -n "$1" ]; then
 
-echo "\n\033[1;32m▶ Running XXMessenger example tests on iOS Simulator...\033[0m"
-set -o pipefail && xcodebuild -workspace 'Examples/xx-messenger/XXMessenger.xcworkspace' -scheme 'XXMessenger' -sdk iphonesimulator -destination 'platform=iOS Simulator,OS=15.5,name=iPhone 13' test | ./xcbeautify
+  echo "\n\033[1;32m▶ Running package tests on iOS Simulator...\033[0m"
+  set -o pipefail && xcodebuild -scheme 'elixxir-dapps-sdk-swift-Package' -sdk iphonesimulator -destination 'platform=iOS Simulator,OS=15.5,name=iPhone 13' test | ./xcbeautify
+
+elif [ "$1" = "examples" ]; then
+
+  echo "\n\033[1;32m▶ Running XXMessenger example tests on iOS Simulator...\033[0m"
+  set -o pipefail && xcodebuild -workspace 'Examples/xx-messenger/XXMessenger.xcworkspace' -scheme 'XXMessenger' -sdk iphonesimulator -destination 'platform=iOS Simulator,OS=15.5,name=iPhone 13' test | ./xcbeautify
+
+else
+
+  echo "Invalid option. Usage:"
+  echo "  run-tests.sh          - Run package tests"
+  echo "  run-tests.sh examples - Run examples tests"
+  exit 1
+
+fi
