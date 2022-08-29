c772ddc853441f653128fa9c7c3f0e902ecf3c0b
Dariusz Rybicki <dariusz@elixxir.io>
2022-08-29 13:02:08 +0100

    Fix run-tests.sh script

--- run-tests.sh
+++ run-tests.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -e
-set -o pipefail && xcodebuild -scheme 'elixxir-dapps-sdk-swift-Package2' -sdk iphonesimulator -destination 'platform=iOS Simulator,OS=15.5,name=iPhone 13' test | ./xcbeautify
+set -o pipefail && xcodebuild -scheme 'elixxir-dapps-sdk-swift-Package' -sdk iphonesimulator -destination 'platform=iOS Simulator,OS=15.5,name=iPhone 13' test | ./xcbeautify
