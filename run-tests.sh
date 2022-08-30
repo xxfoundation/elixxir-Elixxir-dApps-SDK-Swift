803003114d890c112ea6d077a084409cfd84af5d
Dariusz Rybicki <dariusz@elixxir.io>
2022-08-30 01:30:26 +0100

    Update run-tests script

--- run-tests.sh
+++ run-tests.sh
@@ -6,6 +6,9 @@ xcodebuild -version
 
 if [ ! -n "$1" ]; then
 
+  echo "\n\033[1;32m▶ Running package tests on macOS...\033[0m"
+  set -o pipefail && swift test 2>&1 | ./xcbeautify
+
   echo "\n\033[1;32m▶ Running package tests on iOS Simulator...\033[0m"
   set -o pipefail && xcodebuild -scheme 'elixxir-dapps-sdk-swift-Package' -sdk iphonesimulator -destination 'platform=iOS Simulator,OS=15.5,name=iPhone 13' test | ./xcbeautify
 
