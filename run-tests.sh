92739f10ccb38b27fcc83a28b261e805056a017a
Dariusz Rybicki <dariusz@elixxir.io>
2022-08-30 21:22:31 +0100

    Update run-tests script

--- run-tests.sh
+++ run-tests.sh
@@ -14,7 +14,7 @@ elif [ "$1" = "ios" ]; then
   echo "\n\033[1;32m▶ Running package tests on iOS Simulator...\033[0m"
   set -o pipefail && xcodebuild -scheme 'elixxir-dapps-sdk-swift-Package' -sdk iphonesimulator -destination 'platform=iOS Simulator,OS=15.5,name=iPhone 13' test | ./xcbeautify
 
-elif [ "$1" = "ios-examples" ]; then
+elif [ "$1" = "examples-ios" ]; then
 
   echo "\n\033[1;32m▶ Running XXMessenger example tests on iOS Simulator...\033[0m"
   set -o pipefail && xcodebuild -workspace 'Examples/xx-messenger/XXMessenger.xcworkspace' -scheme 'XXMessenger' -sdk iphonesimulator -destination 'platform=iOS Simulator,OS=15.5,name=iPhone 13' test | ./xcbeautify
@@ -24,7 +24,7 @@ else
   echo "\n\033[1;31m▶ Invalid option.\033[0m Usage:"
   echo "  run-tests.sh macos        - Run package tests on macOS"
   echo "  run-tests.sh ios          - Run package tests on iOS Simulator"
-  echo "  run-tests.sh ios-examples - Run iOS examples tests"
+  echo "  run-tests.sh examples-ios - Run iOS examples tests"
   exit 1
 
 fi
