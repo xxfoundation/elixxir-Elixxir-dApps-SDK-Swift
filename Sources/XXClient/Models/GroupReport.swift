1444ea9ace3d016282e6428ca6dff4257808d5f3
Dariusz Rybicki <dariusz@elixxir.io>
2022-09-08 22:54:27 +0200

    Update GroupReport model

--- Sources/XXClient/Models/GroupReport.swift
+++ Sources/XXClient/Models/GroupReport.swift
@@ -4,15 +4,18 @@ public struct GroupReport: Equatable {
   public init(
     id: Data,
     rounds: [Int],
+    roundURL: String,
     status: Int
   ) {
     self.id = id
     self.rounds = rounds
+    self.roundURL = roundURL
     self.status = status
   }
 
   public var id: Data
   public var rounds: [Int]
+  public var roundURL: String
   public var status: Int
 }
 
@@ -20,6 +23,7 @@ extension GroupReport: Codable {
   enum CodingKeys: String, CodingKey {
     case id = "Id"
     case rounds = "Rounds"
+    case roundURL = "RoundURL"
     case status = "Status"
   }
 
