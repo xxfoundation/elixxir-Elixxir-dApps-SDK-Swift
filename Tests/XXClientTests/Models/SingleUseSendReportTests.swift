d8f73a633cf4d870ae9112226766b7810db8d0dd
Dariusz Rybicki <dariusz@elixxir.io>
2022-09-08 23:06:12 +0200

    Update SingleUseSendReport model

--- Tests/XXClientTests/Models/SingleUseSendReportTests.swift
+++ Tests/XXClientTests/Models/SingleUseSendReportTests.swift
@@ -5,11 +5,13 @@ import XCTest
 final class SingleUseSendReportTests: XCTestCase {
   func testCoding() throws {
     let rounds: [Int] = [1, 5, 9]
+    let roundURL = "https://dashboard.xx.network/rounds/25?xxmessenger=true"
     let ephId: Int64 = 1_655_533
     let receptionIdB64 = "emV6aW1hAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD"
     let jsonString = """
     {
       "Rounds": [\(rounds.map { "\($0)" }.joined(separator: ", "))],
+      "RoundURL": "\(roundURL)",
       "EphID": \(ephId),
       "ReceptionID": "\(receptionIdB64)"
     }
@@ -19,6 +21,7 @@ final class SingleUseSendReportTests: XCTestCase {
 
     XCTAssertNoDifference(model, SingleUseSendReport(
       rounds: rounds,
+      roundURL: roundURL,
       ephId: ephId,
       receptionId: Data(base64Encoded: receptionIdB64)!
     ))
