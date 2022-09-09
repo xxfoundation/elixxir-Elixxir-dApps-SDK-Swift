7fdbd1c6248f635ae42837c58737a4f73d28a713
Dariusz Rybicki <dariusz@elixxir.io>
2022-09-09 15:18:32 +0200

    WIP

--- Sources/XXMessengerClient/Messenger/Functions/MessengerVerifyContact.swift
+++ Sources/XXMessengerClient/Messenger/Functions/MessengerVerifyContact.swift
@@ -1,7 +1,13 @@
+import Foundation
 import XCTestDynamicOverlay
 import XXClient
 
 public struct MessengerVerifyContact {
+  public enum Error: Swift.Error, Equatable {
+    case notConnected
+    case notLoggedIn
+  }
+
   public var run: (Contact) throws -> Bool
 
   public func callAsFunction(_ contact: Contact) throws -> Bool {
@@ -12,8 +18,54 @@ public struct MessengerVerifyContact {
 extension MessengerVerifyContact {
   public static func live(_ env: MessengerEnvironment) -> MessengerVerifyContact {
     MessengerVerifyContact { contact in
-      // TODO:
-      return false
+      guard let e2e = env.e2e() else {
+        throw Error.notConnected
+      }
+      guard let ud = env.ud() else {
+        throw Error.notLoggedIn
+      }
+      let facts = try contact.getFacts()
+      let verifiedContact: Contact?
+      if facts.isEmpty {
+        var lookupResult: Result<Contact, NSError>!
+        let semaphore = DispatchSemaphore(value: 0)
+        _ = try env.lookupUD(
+          e2eId: e2e.getId(),
+          udContact: try ud.getContact(),
+          lookupId: try contact.getId(),
+          callback: .init { result in
+            lookupResult = result
+            semaphore.signal()
+          }
+        )
+        semaphore.wait()
+        verifiedContact = try lookupResult.get()
+      } else {
+        var searchResult: Result<[Contact], NSError>!
+        let semaphore = DispatchSemaphore(value: 0)
+        _ = try env.searchUD(
+          e2eId: e2e.getId(),
+          udContact: try ud.getContact(),
+          facts: facts,
+          singleRequestParamsJSON: env.getSingleUseParams(),
+          callback: .init { result in
+            searchResult = result
+            semaphore.signal()
+          }
+        )
+        semaphore.wait()
+        verifiedContact = try searchResult.get().first
+      }
+
+      guard let verifiedContact = verifiedContact else {
+        return false
+      }
+
+      return try e2e.verifyOwnership(
+        received: contact,
+        verified: verifiedContact,
+        e2eId: e2e.getId()
+      )
     }
   }
 }
