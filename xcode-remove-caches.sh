dbf5b0986e5f3def537497f1faba063d99abd8d6
Dariusz Rybicki <dariusz@elixxir.io>
2022-09-20 14:47:37 +0200

    Add xcode-remove-caches script

---
+++ xcode-remove-caches.sh
@@ -0,0 +1,4 @@
+rm -rf "$(getconf DARWIN_USER_CACHE_DIR)/org.llvm.clang/ModuleCache"
+rm -rf "$(getconf DARWIN_USER_CACHE_DIR)/org.llvm.clang.$(whoami)/ModuleCache"
+rm -rf ~/Library/Developer/Xcode/DerivedData/*
+rm -rf ~/Library/Caches/com.apple.dt.Xcode/*
