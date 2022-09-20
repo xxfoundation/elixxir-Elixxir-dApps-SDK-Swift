098730660417b60725ec3cebf88a1d9b37c8d0bf
Dariusz Rybicki <dariusz@elixxir.io>
2022-09-20 14:51:51 +0200

    Update xcode-remove-caches script

--- xcode-remove-caches.sh
+++ xcode-remove-caches.sh
@@ -1,3 +1,5 @@
+pkill -int com.apple.CoreSimulator.CoreSimulatorService
+killall Xcode
 rm -rf "$(getconf DARWIN_USER_CACHE_DIR)/org.llvm.clang/ModuleCache"
 rm -rf "$(getconf DARWIN_USER_CACHE_DIR)/org.llvm.clang.$(whoami)/ModuleCache"
 rm -rf ~/Library/Developer/Xcode/DerivedData/*
