2961ba1c474209ff6f8d9a43af42635c86ced459
Dariusz Rybicki <dariusz@elixxir.io>
2022-09-23 20:17:11 +0200

    Update client-ios-db to v1.2.0

--- Examples/xx-messenger/Package.swift
+++ Examples/xx-messenger/Package.swift
@@ -39,7 +39,7 @@ let package = Package(
     ),
     .package(
       url: "https://git.xx.network/elixxir/client-ios-db.git",
-      .upToNextMajor(from: "1.1.0")
+      .upToNextMajor(from: "1.2.0")
     ),
     .package(
       url: "https://github.com/darrarski/swift-composable-presentation.git",
