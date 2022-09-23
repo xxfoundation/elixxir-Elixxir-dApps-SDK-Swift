7c8e948369baad5d83a0b0d7f0e8b77b0387fc39
Dariusz Rybicki <dariusz@elixxir.io>
2022-09-23 18:57:37 +0200

    Run tests on iOS 16 simulators

--- run-tests.sh
+++ run-tests.sh
@@ -9,12 +9,12 @@ if [ "$1" = "macos" ]; then
 elif [ "$1" = "ios" ]; then
 
   echo "\n\033[1;32m▶ Running package tests on iOS Simulator...\033[0m"
-  set -o pipefail && xcodebuild -scheme 'elixxir-dapps-sdk-swift-Package' -sdk iphonesimulator -destination 'platform=iOS Simulator,OS=15.5,name=iPhone 13' test | ./xcbeautify
+  set -o pipefail && xcodebuild -scheme 'elixxir-dapps-sdk-swift-Package' -sdk iphonesimulator -destination 'platform=iOS Simulator,OS=16.0,name=iPhone 14' test | ./xcbeautify
 
 elif [ "$1" = "examples-ios" ]; then
 
   echo "\n\033[1;32m▶ Running XXMessenger example tests on iOS Simulator...\033[0m"
-  set -o pipefail && xcodebuild -workspace 'Examples/xx-messenger/XXMessenger.xcworkspace' -scheme 'XXMessenger' -sdk iphonesimulator -destination 'platform=iOS Simulator,OS=15.5,name=iPhone 13' test | ./xcbeautify
+  set -o pipefail && xcodebuild -workspace 'Examples/xx-messenger/XXMessenger.xcworkspace' -scheme 'XXMessenger' -sdk iphonesimulator -destination 'platform=iOS Simulator,OS=16.0,name=iPhone 14' test | ./xcbeautify
 
 else
 
