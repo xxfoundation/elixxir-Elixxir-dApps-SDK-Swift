b81e0b4bddff2a282c073a4cdb06a1d3f678bd32
Dariusz Rybicki <dariusz@elixxir.io>
2022-09-23 17:50:59 +0200

    Remove xcbeautify

--- run-tests.sh
+++ run-tests.sh
@@ -4,17 +4,17 @@ set -e
 if [ "$1" = "macos" ]; then
 
   echo "\n\033[1;32m▶ Running package tests on macOS...\033[0m"
-  set -o pipefail && swift test 2>&1 | ./xcbeautify
+  swift test
 
 elif [ "$1" = "ios" ]; then
 
   echo "\n\033[1;32m▶ Running package tests on iOS Simulator...\033[0m"
-  set -o pipefail && xcodebuild -scheme 'elixxir-dapps-sdk-swift-Package' -sdk iphonesimulator -destination 'platform=iOS Simulator,OS=15.5,name=iPhone 13' test | ./xcbeautify
+  xcodebuild -scheme 'elixxir-dapps-sdk-swift-Package' -sdk iphonesimulator -destination 'platform=iOS Simulator,OS=15.5,name=iPhone 13' test
 
 elif [ "$1" = "examples-ios" ]; then
 
   echo "\n\033[1;32m▶ Running XXMessenger example tests on iOS Simulator...\033[0m"
-  set -o pipefail && xcodebuild -workspace 'Examples/xx-messenger/XXMessenger.xcworkspace' -scheme 'XXMessenger' -sdk iphonesimulator -destination 'platform=iOS Simulator,OS=15.5,name=iPhone 13' test | ./xcbeautify
+  xcodebuild -workspace 'Examples/xx-messenger/XXMessenger.xcworkspace' -scheme 'XXMessenger' -sdk iphonesimulator -destination 'platform=iOS Simulator,OS=15.5,name=iPhone 13' test
 
 else
 
