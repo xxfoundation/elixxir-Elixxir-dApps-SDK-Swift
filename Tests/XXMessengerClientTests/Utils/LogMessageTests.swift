cd7f9c8c4c6f3d58a7ae8b2b882d8184f2d48e56
Dariusz Rybicki <dariusz@elixxir.io>
2022-10-05 01:48:25 +0200

    Improve LogeMessage parsing

--- Tests/XXMessengerClientTests/Utils/LogMessageTests.swift
+++ Tests/XXMessengerClientTests/Utils/LogMessageTests.swift
@@ -40,4 +40,19 @@ final class LogMessageTests: XCTestCase {
       LogMessage(level: .notice, text: "1234 Wrongly formatted")
     )
   }
+
+  func testParsingStripsDateTime() {
+    XCTAssertNoDifference(
+      LogMessage.parse("INFO 2022/10/04 Informing..."),
+      LogMessage(level: .info, text: "Informing...")
+    )
+    XCTAssertNoDifference(
+      LogMessage.parse("INFO 23:36:55.755390 Informing..."),
+      LogMessage(level: .info, text: "Informing...")
+    )
+    XCTAssertNoDifference(
+      LogMessage.parse("INFO 2022/10/04 23:36:55.755390 Informing..."),
+      LogMessage(level: .info, text: "Informing...")
+    )
+  }
 }
