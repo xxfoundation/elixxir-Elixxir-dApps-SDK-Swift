1a6e4ad5e8c13110e362338731b3583b8c305162
Dariusz Rybicki <dariusz@elixxir.io>
2022-11-08 23:28:23 +0100

    Add Stored.userDefaults implementation

--- Tests/XXMessengerClientTests/Utils/StoredTests.swift
+++ Tests/XXMessengerClientTests/Utils/StoredTests.swift
@@ -1,8 +1,9 @@
+import CustomDump
 import XCTest
 @testable import XXMessengerClient
 
 final class StoredTests: XCTestCase {
-  func testInMemory() throws {
+  func testInMemory() {
     let stored: Stored<String?> = .inMemory()
 
     XCTAssertNil(stored())
@@ -14,5 +15,36 @@ final class StoredTests: XCTestCase {
     stored.set(nil)
 
     XCTAssertNil(stored())
+  }
+
+  func testUserDefaults() {
+    struct Value: Equatable, Codable {
+      var id = UUID()
+    }
+
+    let key = "key"
+    let userDefaults = UserDefaults(suiteName: "XXMessengerClient_StoredTests")!
+    userDefaults.removeObject(forKey: key)
+    let stored: Stored<Value?> = .userDefaults(
+      key: key,
+      userDefaults: userDefaults
+    )
+
+    XCTAssertNoDifference(stored.get(), nil)
+
+    let value = Value()
+    stored.set(value)
+
+    XCTAssertNoDifference(
+      userDefaults.data(forKey: key).map { data in
+        try? JSONDecoder().decode(Value.self, from: data)
+      },
+      value
+    )
+    XCTAssertNoDifference(stored.get(), value)
+
+    userDefaults.set(nil, forKey: key)
+
+    XCTAssertNoDifference(stored.get(), nil)
   }
 }
