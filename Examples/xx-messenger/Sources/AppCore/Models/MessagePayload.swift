c01336a3ca18d1de814440ea798155acdb2d3be3
Dariusz Rybicki <dariusz@elixxir.io>
2022-11-29 10:26:30 +0100

    Update MessagePayload model in messenger example app

--- Examples/xx-messenger/Sources/AppCore/Models/MessagePayload.swift
+++ Examples/xx-messenger/Sources/AppCore/Models/MessagePayload.swift
@@ -1,16 +1,22 @@
 import Foundation
 
 public struct MessagePayload: Equatable {
-  public init(text: String) {
+  public init(
+    text: String,
+    replyingTo: Data? = nil
+  ) {
     self.text = text
+    self.replyingTo = replyingTo
   }
 
   public var text: String
+  public var replyingTo: Data?
 }
 
 extension MessagePayload: Codable {
   enum CodingKeys: String, CodingKey {
     case text
+    case replyingTo
   }
 
   public static func decode(_ data: Data) throws -> Self {
